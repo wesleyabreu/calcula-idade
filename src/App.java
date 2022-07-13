17508b1293f79696c851823eae3e6e15f79a3bfa
Wesley Henrique Santos Abreu <wesleyhenriquesantosabreu@gmail.com>
2022-07-13 18:35:01 -0300

    Ano atual agora é automático.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("Em que ano você nasceu? ");
         int ano = Integer.parseInt(entrada.nextLine());
-        int idadeEsseAno = 2022 - ano;
+        int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+        int idadeEsseAno = anoAtual - ano;
         System.out.print("Este ano você faz " + idadeEsseAno + " anos.");
         entrada.close();
     }
